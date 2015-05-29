a3e368d647cd8142d9947161f3bc3745c79d2439
Nikolay Gromov <nicothin@gmail.com>
2015-05-29 20:24:24 +0300

    Start

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+
+
+
+  require('load-grunt-tasks')(grunt);
+
+
+
+  grunt.initConfig({
+
+
+
+    less: {
+      style: {
+        files: {
+          'css/style.css': ['less/style.less']
+        }
+      }
+    },
+
+
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      style: {
+        src: 'css/style.css'
+      }
+    },
+
+
+
+    cmq: {
+      style: {
+        files: {
+          'css/style.css': ['css/style.css']
+        }
+      }
+    },
+
+
+
+    watch: {
+      style: {
+        files: ['less/**/*.less'],
+        tasks: ['style'],
+        options: {
+          spawn: false,
+          livereload: true
+        },
+      },
+      html: {
+        files: ['*.html'],
+        options: {
+          spawn: false,
+          livereload: true
+        },
+      },
+    },
+
+
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            'css/*.css',
+            'js/*.js',
+            '*.html',
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            // baseDir: "build/",
+          },
+          notify: false,
+          startPath: "/index.html",
+          ghostMode: {
+            clicks: true,
+            forms: true,
+            scroll: false
+          }
+        }
+      }
+    }
+
+  });
+
+
+
+  grunt.registerTask('default', [
+    'less',
+    'autoprefixer',
+    'cmq',
+    'browserSync',
+    'watch',
+  ]);
+
+
+
+  grunt.registerTask('style', [
+    'less',
+    'autoprefixer',
+    'cmq',
+  ]);
+
+};
